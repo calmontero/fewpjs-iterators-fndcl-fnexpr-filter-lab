ba8f3cd863af8402568e131375394374bf729bfd
Carlos <carlos.alfonso.montero@gmail.com>
2021-04-05 21:12:03 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers,name) {
+    return drivers.filter( x =>
+        x.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(name.toLowerCase()) === 0)
+}
+
+function matchName(drivers, name) {
+    return drivers.filter( record => record.name === name);
+}
